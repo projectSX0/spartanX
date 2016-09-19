b2d7b9f7c0fd2378f7dd5cfc06e6f98608172fb7
yuji <hatsuneyuji@yahoo.co.jp>
2016-09-19 02:13:15 -0700

    test kernel

--- Sources/FileEvents.swift
+++ Sources/FileEvents.swift
@@ -8,17 +8,18 @@
 
 import Foundation
 
+#if !os(Linux)
 extension SXKernel {
     public struct FileEvents: OptionSet {
-        #if os(Linux)
-        static let deleted = FileEvents(rawValue: UInt32(IN_DELETE))
-        static let written = FileEvents(rawValue: UInt32(IN_MODIFY))
-//        static let sizeChanged = FileEvents(rawValue: UInt32(IN_MODIFY))
-//        static let permissionChanged = FileEvents(rawValue: UInt32(IN_ATTRIB))
-//        static let linked = FileEvents(rawValue: UInt32(NOTE_LINK))
-//        static let renamed = FileEvents(rawValue: UInt32(NOTE_RENAME))
-//        static let revoked = FileEvents(rawValue: UInt32(NOTE_REVOKE))
-        #else
+//        #if os(Linux)
+//        static let deleted = FileEvents(rawValue: UInt32(IN_DELETE))
+//        static let written = FileEvents(rawValue: UInt32(IN_MODIFY))
+////        static let sizeChanged = FileEvents(rawValue: UInt32(IN_MODIFY))
+////        static let permissionChanged = FileEvents(rawValue: UInt32(IN_ATTRIB))
+////        static let linked = FileEvents(rawValue: UInt32(NOTE_LINK))
+////        static let renamed = FileEvents(rawValue: UInt32(NOTE_RENAME))
+////        static let revoked = FileEvents(rawValue: UInt32(NOTE_REVOKE))
+//        #else
         static let deleted = FileEvents(rawValue: UInt32(NOTE_DELETE))
         static let written = FileEvents(rawValue: UInt32(NOTE_WRITE))
         static let sizeChanged = FileEvents(rawValue: UInt32(NOTE_EXTEND))
@@ -26,7 +27,7 @@ extension SXKernel {
         static let linked = FileEvents(rawValue: UInt32(NOTE_LINK))
         static let renamed = FileEvents(rawValue: UInt32(NOTE_RENAME))
         static let revoked = FileEvents(rawValue: UInt32(NOTE_REVOKE))
-        #endif
+//        #endif
         public typealias RawValue = u_int
         
         public var rawValue: UInt32
@@ -40,14 +41,14 @@ extension SXKernel {
 extension SXKernel {
     public func monitor(file: String, events: FileEvents, callback: @escaping (String, FileEvents) -> Bool) {
         
-        #if os(Linux)
-        let fd = inotify_init()
-        _ = inotify_add_watch(fd, file, events.rawValue)
-        var ev = epoll_event()
-        ev.events = EPOLLOUT.rawValue | EPOLLIN.rawValue | EOILLET.rawValue
-        ev.data.fd = fd
-        epoll_ctl(kq, EPOLL_CTL_ADD, fd, &ev)
-        #else
+//        #if os(Linux)
+//        let fd = inotify_init()
+//        _ = inotify_add_watch(fd, file, events.rawValue)
+//        var ev = epoll_event()
+//        ev.events = EPOLLOUT.rawValue | EPOLLIN.rawValue | EOILLET.rawValue
+//        ev.data.fd = fd
+//        epoll_ctl(kq, EPOLL_CTL_ADD, fd, &ev)
+//        #else
         let fd = open(file, O_RDONLY)
         var k = event(ident: UInt(fd),
                       filter: Int16(EVFILT_VNODE),
@@ -56,7 +57,7 @@ extension SXKernel {
                       data: 0,
                       udata: nil)
             kevent(fd, &k, 1, nil, 0, nil)
-        #endif
+//        #endif
         
         self.queues[fd] = FileEventHandler(path: file, ident: fd, callback: callback)
     
@@ -82,3 +83,4 @@ internal struct FileEventHandler: KqueueManagable {
     }
 }
 
+#endif
