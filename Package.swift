391984fc98d8dd2161f6af9f6c3d1eba299ed78a
Yuji <hatsuneyuji@yahoo.co.jp>
2017-02-13 18:15:31 -0800

    added network interface api

--- Package.swift
+++ Package.swift
@@ -2,11 +2,12 @@ import PackageDescription
 
 var package = Package(
     name: "spartanX",
+    targets: [
+        Target(name: "spartanX", dependencies: ["spartanX-include"]),
+        Target(name: "spartanX-include", dependencies: [])
+    ],
     dependencies: [.Package(url: "https://github.com/michael-yuji/FoundationPlus.git", versions: Version(0,0,0)..<Version(1,0,0)),
                    .Package(url: "https://github.com/michael-yuji/CKit.git", versions: Version(0,0,0)..<Version(1,0,0)),
                    .Package(url: "https://github.com/projectSX0/XThreads.git", versions: Version(0,0,0)..<Version(1,0,0))]
 )
 
-#if os(Linux)
-    package.dependencies.append(.Package(url: "https://github.com/projectSX0/spartanX-linux-include.git", versions: Version(0,0,0)..<Version(1,0,0)))
-#endif
