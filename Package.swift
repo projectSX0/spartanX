0c489703e0d83fc13fe075d4484fc0ccdcdcb9c2
michael-yuji <michael-yuji@users.noreply.github.com>
2017-04-15 17:53:17 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ var package = Package(
     name: "spartanX",
     dependencies: [.Package(url: "https://github.com/michael-yuji/FoundationPlus.git", versions: Version(0,0,0)..<Version(1,0,0)),
                    .Package(url: "https://github.com/michael-yuji/CKit.git", versions: Version(0,0,0)..<Version(1,0,0)),
-                   .Package(url: "https://github.com/projectSX0/XThreads.git", versions: Version(0,0,0)..<Version(1,0,0))]
+                   .Package(url: "https://github.com/projectSX0/XThreads.git", versions: Version(0,0,0)..<Version(0,0,3))]
 )
 
